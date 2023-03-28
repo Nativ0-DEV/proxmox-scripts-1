489fbbc8b909c31be47755909782c5914d939eb7
Angelin Miranda <angelincrm@gmail.com>
2023-03-28 10:28:28 -0300

    Update setup.sh

--- lxc/nginx-proxy-manager/setup.sh
+++ lxc/nginx-proxy-manager/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 TMP=/tmp/npm_install.sh
-URL=https://raw.githubusercontent.com/Nativ0-DEV/proxmox-scripts-1/main/lxc/nginx-proxy-manager/install
+URL=https://github.com/n4tiv0/proxmox-scripts1/tree/main/lxc/nginx-proxy-manager/install
 
 if [ "$(uname)" != "Linux" ]; then
   echo "OS NOT SUPPORTED"
